4a384173c7d5898e349d037b78736b0a2a702869
Burhan ul haq <burhanulhaqsandhu2432@gmail.com>
2024-10-12 18:50:09 +0500

    deploying project

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn VidSummarize.wsgi --log-file -
--- VidSummarize/settings.py
+++ VidSummarize/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-#$6e%uzjym7v5-_d%oz23xlxr8s+^k2y@ykne-%()-i1nj&-bl
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['VidSummarize.herokuapp.com']
 
 
 # Application definition
@@ -122,3 +122,6 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import django_heroku
+django_heroku.settings(locals())
